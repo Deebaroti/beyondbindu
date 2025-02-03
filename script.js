eff18eef97455692abc8824b6a80f02b05f55243
Deebaroti <deebarotidey02@gmail.com>
2025-02-03 23:25:03 +0530

    fixed the hero section

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const menuOpenButton = document.querySelector("#menu-open-button")
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click",()=>{
     //Toggle Mobile Menu Visibility
     document.body.classList.toggle("show-mobile-menu");
-})
+});
+
+// close menu when the closed button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click
+());
