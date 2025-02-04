00ec311bb051b932ba5861cb8ec9700eae1d7767
Deebaroti <deebarotidey02@gmail.com>
2025-02-04 16:31:39 +0530

    complete all sections

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -9,6 +10,11 @@ menuOpenButton.addEventListener("click",()=>{
 // close menu when the closed button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click
 ());
+
+//close menu when the navlink is clicked
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
 
 //initialize swiper
 const swiper = new Swiper('.slider-wrapper', {
